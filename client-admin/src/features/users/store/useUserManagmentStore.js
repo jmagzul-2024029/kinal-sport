32ade7dd902292447965c12c1fe47820f9fdd0bd
jmagzul-2024029 <jmagzul-2024029@kinal.edu.gt>
2026-04-23 11:54:00 -0600

    Se agregan avances del proyecto para poder actualizar

--- client-admin/src/features/users/store/useUserManagmentStore.js
+++ client-admin/src/features/users/store/useUserManagmentStore.js
@@ -1,7 +1,8 @@
-import { create } from "zustand"
-import * as authApi from "../../../shared/api/auth.js"
+import { create } from "zustand";
+import * as authApi from "../../../shared/api/auth.js";
 
 const getAllUsers = authApi.getAllUsers;
+const updateUserRoleRequest = authApi.updateUserRole;
 
 export const useUserManagementStore = create((set, get) => ({
     users: [],
@@ -13,30 +14,57 @@ export const useUserManagementStore = create((set, get) => ({
 
     setUsers: (users) => set({ users }),
 
-    fetchUsers: async (apiFn = getAllUsers, options = {}) => {
-
-        const { force = false } = options;
-
-        const state = get();
-
-        //Evitar llamadas duplicadas.
-        if(state.loading) return;
-
-        //Por si ya estan cargados, no volver a pedir a menos que se fuerce.
-        if(!force && state.users.length > 0) return;
-
-        set({ loading: true, error: null })
-
+    updateUserRole: async (userId, newRole) => {
+        set({ loading: true, error: null });
         try {
-            
-            const fetcher = typeof apiFn === "function" ? apiFn : getAllUsers;
-
-            const result = await fetcher();
-
-            set({ users: result.users || result, loading: false})
-
-        } catch (err) {
-            set({ error: err.message || "Error al cargar usuarios", loading: false})
+        if (typeof updateUserRoleRequest !== "function") {
+            throw new Error("Función de updateUserRole para actualizar rol no disponible",);
         }
+
+    const { data: updateUser } = await updateUserRoleRequest(userId, newRole);
+
+    const users = get().users.map((u) =>
+        u.id === updateUser.id ? { ...u, role: updateUser.role } : u,
+    );
+
+    set({ users, loading: false });
+    return { success: true, user: updateUser };
+
+    } catch (err) {
+        set({
+            error:
+                err.response?.data?.message ||
+                err.message ||
+                "Error al actualizar el rol del usuario",
+                loading: false,
+        });
+        return {
+        success: false,
+        error: err.response?.data?.message || err.message,
+        };
     }
-}))
+    },
+
+    fetchUsers: async (apiFn = getAllUsers, options = {}) => {
+    const { force = false } = options;
+
+    const state = get();
+
+    //Evitar llamadas duplicadas.
+    if (state.loading) return;
+
+    //Por si ya estan cargados, no volver a pedir a menos que se fuerce.
+    if (!force && state.users.length > 0) return;
+
+    set({ loading: true, error: null });
+
+    try {
+        const fetcher = typeof apiFn === "function" ? apiFn : getAllUsers;
+        const result = await fetcher();
+
+        set({ users: result.users || result, loading: false });
+    } catch (err) {
+        set({ error: err.message || "Error al cargar usuarios", loading: false });
+    }
+},
+}));
